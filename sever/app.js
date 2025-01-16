9271adaed9fb52da6c962bee064ae1ba8dae3745
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-01-16 11:56:08 +0530

    added

--- sever/app.js
+++ sever/app.js
@@ -58,7 +58,7 @@ app.get('/auth/google/callback',
 import authRoutes from "./routes/auth.routes.js"
 app.use("/api/auth", authRoutes)
 app.get("/bnm", (req, res, next) => {
-    res.redirect('https://notes-creating-frontend.onrender.com/redirecting-to-dashbord');
+    res.redirect('https://notes-creating-frontend.onrender.com');
 })
 
 
